6e372a56723d52c9fda06084017d9437661e8178
nflguy007 <129890651+nflguy007@users.noreply.github.com>
2023-04-05 18:44:16 +0000

    Updated Q2

--- pages/Question2.tsx
+++ pages/Question2.tsx
@@ -42,7 +42,11 @@ export default function Home() {
         <div className={styles.center}>
         <p className={inter.className}>
                           Q2 - Describe how you solved a challenge that one of your previous support teams faced. How did you determine your solution was successful? 
-                          A:   
+                          A:  I notices a situation where we would recieve a batch of monthy tickets from random customers that were failing their SSO authentication.
+                          After taking these tickets, and finding that the majority of them were due to the authentication certificate expiring, I worked with our Authentication Team
+                          to automate an email to the admins of the environment one month, and a reminder email 15 days before the certificate expired. 
+                          I would also get a report of the monthly emails sent, so I could spot check to make sure SSO certificates were updated.
+                          I measured success on the fact that this grouping of tickets were no longer created monthly. 
         </p>
         </div>
 
