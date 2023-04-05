13fb59002dcdf0dbe9a3fd568bff27408d36d83d
nflguy007 <129890651+nflguy007@users.noreply.github.com>
2023-04-05 18:31:08 +0000

    Esacaped the ' with a &#39;

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,8 +42,8 @@ export default function Home() {
         <div className={styles.center}>
         <p className={inter.className}>
                           Q1 - What do you want to learn or do more of at work? 
-                          A: Problem solve. I love a new problem, or puzzle to figure out. I'm always looking something new to learn, or product to test.
-                          I enjoy being able to spend some the time to learn something from top to bottom, really understand how/why it works.  
+                          A: Problem solve. I love a new problem, or puzzle to figure out. I&#39;m always looking something new to learn, or product to test.
+                          I enjoy being able to spend some the time to learn something from top to bottom, really understand how and why it works.  
         </p>
         </div>
 
