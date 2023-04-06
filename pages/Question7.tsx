77e79dc52873d097c1e93ca7ae15f521badad9cf
nflguy007 <129890651+nflguy007@users.noreply.github.com>
2023-04-06 04:57:06 +0000

    Spelling correction

--- pages/Question7.tsx
+++ pages/Question7.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className={styles.center}>
         <p className={inter.className}>
                         Q7 -  How could we improve or alter this familiarization exercise?
-                        A - As you can tell, the one thing I couldn&#39;t figure out was now to index and actually build secondary pages. 
+                        A - As you can tell, the one thing I couldn&#39;t figure out was how to index and actually build secondary pages. 
                         I believe that this is just my lack of knowledge and experience.  I&#39;ll be the first to admit, that there would be a pretty
                         steep learning curve for me to take on this role.  I totally enjoyed the experience, and learning process I endured to get this
                         as completed as I can.  I honestly don&#39;t think I&#39;d change the assessment much at all.                            
